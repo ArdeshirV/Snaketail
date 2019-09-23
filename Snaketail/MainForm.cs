6cf1e273752a00add4667232699d9d9b7efb9c52
Ardeshir <ardeshirv@protonmail.com>
2019-09-23 19:54:51 +0330

    Upload files

---
+++ Snaketail/MainForm.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2019 ArdeshirV@protonmail.com & ...
+// Licensed under GPLv3+
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Snaketail
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Snaketail/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Asha
+ * Date: 9/21/2019
+ * Time: 9:50 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Snaketail
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
